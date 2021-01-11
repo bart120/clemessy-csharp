715417e969de07ad1cef3b39fda5c794fe87b982
Vincent <vleclerc@inow.fr>
2021-01-11 12:07:18 +0100

    ajouter un contact

---
+++ GestionContact/GestionContact/Metier/Contact.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace GestionContact.Metier
+{
+    public class Contact
+    {
+        public Contact()
+        {
+            this.DateNaissance = DateTime.Now;
+                
+        }
+
+        public Contact(string nom):this()
+        {
+            this.Nom = nom;
+        }
+
+        public Contact(string nom, string prenom):this(nom)
+        {
+            this.Prenom = prenom;
+        }
+
+        ~Contact()
+        {
+            // destructeur
+        }
+
+        private string _nom;
+
+        public string Nom
+        {
+            get { return _nom; }
+            set { _nom = value; }
+        }
+
+        public string Prenom { get; set; }
+
+        public DateTime DateNaissance { get; set; }
+
+        private string _email;
+
+        public string Email
+        {
+            get { return _email; }
+            set {
+                Regex regex = new Regex(@"[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?");
+                if (regex.IsMatch(value))
+                    _email = value;
+                else
+                    throw new FormatException("L'email n'est pas au bon format.");
+            }
+        }
+
+        public void Enregistrer()
+        {
+            string chemin = ConfigurationManager.AppSettings["chemin_fichier"];
+            using (StreamWriter sw = new StreamWriter(chemin, true, Encoding.UTF8))
+            {
+                sw.WriteLine(this.ToString());
+            }
+        }
+
+        public override string ToString()
+        {
+            string resultat = string.Format("{0};{1};{2};{3}", Nom, Prenom, Email, DateNaissance.ToString("dd/MM/yyyy"));
+            return resultat;
+        }
+
+    }
+}
--- GestionContact/GestionContact/Program.cs
+++ GestionContact/GestionContact/Program.cs
@@ -1,4 +1,6 @@
-﻿using System.Text;
+﻿using GestionContact.Metier;
+using System;
+using System.Text;
 
 namespace GestionContact
 {
@@ -6,10 +8,66 @@ namespace GestionContact
     {
         static void Main(string[] args)
         {
+            while (true)
+            {
+                afficherMenu();
+                Console.WriteLine("Votre choix ?");
+                string choix = Console.ReadLine();
 
-            
+                switch (choix)
+                {
+                    case "1":
+                        ajouterContact();
+                        break;
+                    case "2":
+                        listerContact();
+                        break;
+                    case "q":
+                        return;
+                    default:
+                        Console.WriteLine("Choix incorrect.");
+                        break;
+                }
+            }
         }
 
-        
+        private static void ajouterContact()
+        {
+            Contact c = new Contact();
+
+            Console.WriteLine("Nom ?");
+            c.Nom = Console.ReadLine();
+
+            Console.WriteLine("Prénom ?");
+            c.Prenom = Console.ReadLine();
+
+            Console.WriteLine("Email ?");
+            c.Email = Console.ReadLine();
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Date de naissance ?");
+                    c.DateNaissance = Convert.ToDateTime(Console.ReadLine());
+                    break;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("Erreur de saisie!");
+                }
+            }
+            c.Enregistrer();
+            Console.WriteLine("Contact enregistré");
+        }
+
+        private static void afficherMenu()
+        {
+            Console.WriteLine("-- MENU --");
+            Console.WriteLine("1- Ajouter contact");
+            Console.WriteLine("2- Lister les contacts");
+            Console.WriteLine("q- Quitter");
+        }
+
     }
 }
